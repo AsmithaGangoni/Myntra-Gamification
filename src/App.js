afc825b6006a64fcf99aec76f0113cd90c07cc24
AsmithaGangoni <asmithagangoni@gmail.com>
2024-07-15 18:49:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/AuthForm';
+import MainPage from './components/MainPage';
+import Quiz from './components/Quiz';
+import Leaderboard from './components/Leaderboard';
+import Streak from './components/Streak';
+import PrivateRoute from './components/PrivateRoute'; 
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+        <Routes>
+        <Route path="/" element={<Login />} />
+          <Route path="/main" element={<PrivateRoute element={MainPage} />} />
+          <Route path="/quiz" element={<PrivateRoute element={Quiz} />} />
+          <Route path="/leaderboard" element={<PrivateRoute element={Leaderboard} />} />
+          <Route path="/streak" element={<PrivateRoute element={Streak} />} />
+        </Routes>
+        
+      
+    </Router>
   );
-}
+};
 
 export default App;
